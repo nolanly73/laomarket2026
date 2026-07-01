272fba4b4b2f3652adad5c5b4680e2d4b5e9961c
nolanly73 <nolan.ly73@gmail.com>
2026-07-01 22:37:30 +0700

    fix main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,11 @@
-{
-  "compilerOptions": {
-    "target": "ES2020",
-    "useDefineForClassFields": true,
-    "lib": ["ES2020", "DOM", "DOM.Iterable"],
-    "module": "ESNext",
-    "skipLibCheck": true,
-    "moduleResolution": "bundler",
-    "allowImportingTsExtensions": true,
-    "resolveJsonModule": true,
-    "isolatedModules": true,
-    "noEmit": true,
-    "jsx": "react-jsx",
-    "strict": false,
-    "noUnusedLocals": false,
-    "noUnusedParameters": false,
-    "allowArbitraryExtensions": true
-  },
-  "include": ["src"]
-}
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+// @ts-ignore
+import './index.css'
+import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
