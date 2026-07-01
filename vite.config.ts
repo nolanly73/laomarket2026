4c81c21504289c2edb9e5848543af52f0f6d1284
nolanly73 <nolan.ly73@gmail.com>
2026-07-01 22:58:36 +0700

    fix ios safari compatibility

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'es2015',
+    cssTarget: 'chrome61'
+  },
+  esbuild: {
+    target: 'es2015'
+  }
 })
